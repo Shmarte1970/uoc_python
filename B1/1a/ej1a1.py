ec733dea39bb288fbe4fc877dae61f333029b2cc
Shmarte1970 <shmarte@gmail.com>
2025-12-22 17:39:02 +0100

    primer commit ejercicios a1b1c1

---
+++ B1/1a/ej1a1.py
@@ -0,0 +1,67 @@
+'''
+Enunciado:
+Implementa la función 'fibonacci(fibonacci_number)' que contenga el algoritmo
+de Fibonacci y reciba como parámetro un valor numérico entero llamado 
+'fibonacci_number'  y devuelva el valor de la serie Fibonacci en esa posición.
+Asimismo, si el valor no es numérico, o es menor a cero, se debe lanzar 
+una excepción ValueError("mensaje"), la cual puede incluir un mensaje que debería 
+corresponder con el tipo de error durante la validación.
+
+Parámetros:
+- fibonacci_number: Número entero positivo mayor a 0 que representa la
+posición en la serie Fibonacci.
+
+Ejemplo:
+    Entrada:
+    fibonacci(10)
+
+    Salida:
+    55
+
+Enunciat:
+Implementa la funció 'fibonacci(fibonacci_number)' que contingui l'algorisme
+de Fibonacci i rebi com a paràmetre un valor numèric enter anomenat
+'fibonacci_number' i torneu el valor de la sèrie Fibonacci en aquesta posició.
+Així mateix, si el valor no és numèric, o és menor a zero, cal llançar
+una excepció ValueError("missatge"), la qual pot incloure un missatge que hi hauria de
+correspondre amb el tipus d'error durant la validació.
+
+Paràmetres:
+- fibonacci_number: Nombre enter positiu superior a 0 que representa la
+posició a la sèrie Fibonacci.
+
+Exemple:
+     Entrada:
+     fibonacci(10)
+
+     Sortida:
+     55
+
+'''
+
+def fibonacci(fibonacci_number):   
+    # Compruevo que el valor introducido es numerico
+    if not isinstance(fibonacci_number, int):
+        raise ValueError("El valor debe ser un número entero")
+
+    # Compruebo que el valor es mayor que 0
+    if fibonacci_number < 0:
+        raise ValueError("El valor debe ser mayor o igual a 0")
+
+    # Secuencia if para la definicion de la serie Fibonacci
+    if fibonacci_number == 0:
+        return 0
+    if fibonacci_number == 1:
+        return 1
+
+    # Algoritmo de Fibonacci
+    a, b = 0, 1
+    for _ in range(2, fibonacci_number + 1):
+        a, b = b, a + b
+
+    return b
+    pass
+
+# Si quieres probar tu código, descomenta las siguientes líneas y ejecuta el script 
+# Si vols provar el teu codi, descomenta les línies següents i executa l'script
+# print(fibonacci(10))
---
+++ B1/1a/ej1a2.py
@@ -0,0 +1,67 @@
+'''
+Enunciado:
+Crea una función 'sum_odd_numbers(list_numbers)' que reciba como 
+parámetro una lista de números positivos enteros llamada 'list_numbers'
+y devuelva la suma de los números impares encontrados en la lista.
+Considerar que 'list_numbers' debe contener valores numéricos enteros mayores
+o iguales a '0', en caso contrario se debe mostrar un error tipo 'ValueError'.
+
+El error lo puedes mostrar con la siguiente instrucción:    
+raise ValueError("MENSAJE DE ERROR")
+
+Parámetros:
+- list_numbers: Lista de números enteros positivos.
+
+Ejemplo:
+    Entrada:
+    sum_odd_numbers([1, 2, 3, 4, 5, 10, 21, 100])
+
+    Salida:
+    30
+
+Enunciat:
+Crea una funció 'sum_odd_numbers(list_numbers)' que rebi com
+paràmetre una llista de nombres positius enters anomenada 'list_numbers'
+i torneu la suma dels números imparells trobats a la llista.
+Considereu que 'list_numbers' ha de contenir valors numèrics enters majors
+o iguals a '0', en cas contrari cal mostrar un error tipus 'ValueError'.
+
+L'error el pots mostrar amb la següent instrucció:
+raise ValueError("MISSATGE D'ERROR")
+
+Paràmetres:
+- list_numbers: Llista de nombres enters positius.
+
+Exemple:
+     Entrada:
+     sum_odd_numbers([1, 2, 3, 4, 5, 10, 21, 100])
+
+     Sortida:
+     30
+
+'''
+
+def sum_odd_numbers(list_numbers):
+    
+    total = 0
+
+    for number in list_numbers:
+        # El numero tiene que ser entero
+        if not isinstance(number, int):
+            raise ValueError("Todos los elementos deben ser números enteros")
+
+        # El numero tiene que ser >= 0
+        if number < 0:
+            raise ValueError("Todos los elementos deben ser mayores o iguales a 0")
+
+        # Sumo solo los numeros impares
+        if number % 2 != 0:
+            total += number
+
+    return total
+
+    pass
+
+# Si quieres probar tu código, descomenta las siguientes líneas y ejecuta el script 
+# Si vols provar el teu codi, descomenta les línies següents i executa l'script
+# print(sum_odd_numbers([1, 2, 3, 4, 5, 10, 21, 100]))
