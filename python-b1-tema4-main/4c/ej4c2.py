0cfbab782598a84a259d52262ca8de2f9cecbdc6
Shmarte1970 <shmarte@gmail.com>
2026-01-05 12:28:55 +0100

    python-b1-tema4

---
+++ python-b1-tema4-main/4c/ej4c2.py
@@ -0,0 +1,75 @@
+"""
+Enunciado:
+Se pide crear una interfaz "Vehicles" que tenga un método abstracto "drive".
+Además, se deben crear las clases concretas "Car" y "Bicycle" que implementen
+la interfaz "Vehicles".
+
+El método "drive" debe imprimir "Driving a car" para la clase "Car" y "Riding
+a bicycle" para la clase "Bicycle".
+
+Parámetros:
+    La clase Car y Bicycle no reciben parámetros.
+        
+Ejemplo:
+    Entrada:
+        car = Car()
+        print(car.drive())
+
+        bicycle = Bicycle()
+        print(bicycle.drive())
+    Salida:
+        Driving a car
+        Riding a bicycle
+
+
+Enunciat:
+Es demana crear una interfície "Vehicles" que tingui un mètode abstracte "drive".
+A més, cal crear les classes concretes "Car" i "Bicycle" que implementin
+la interfície "Vehicles".
+
+El mètode "drive" ha d'imprimir "Driving a car" per a la classe "Car" i "Riding"
+a bicycle" per a la classe "Bicycle".
+
+Paràmetres:
+     La classe Car i Bicycle no reben paràmetres.
+        
+Exemple:
+     Entrada:
+         car = Car()
+         print(car.drive())
+
+         bicycle = Bicycle()
+         print(bicycle.drive())
+     Sortida:
+         Driving a car
+         Riding a bicycle
+"""
+
+from abc import ABC, abstractmethod
+
+# Abstract class Vehicles
+class Vehicles(ABC):
+    @abstractmethod
+    def drive(self):
+        pass
+
+
+# Class Car implementing Vehicles
+class Car(Vehicles):
+    def drive(self):
+        return "Driving a car"
+
+
+# Class Bicycle implementing Vehicles
+class Bicycle(Vehicles):
+    def drive(self):
+        return "Riding a bicycle"
+
+
+# Si quieres probar tu código, descomenta las siguientes líneas y ejecuta el script
+# Si vols provar el teu codi, descomenta les línies següents i executa l'script
+# car = Car()
+# print(car.drive())
+
+# bicycle = Bicycle()
+# print(bicycle.drive())
---
+++ python-b1-tema4-main/4d/ej4d2.py
@@ -0,0 +1,54 @@
+"""
+Enunciado:
+Implementa la función 'create_read_file()', que no recibe ningún
+parámetro debido a que esta función debe crear un archivo de texto 
+'text_file.txt', dentro de dicho archivo se deben escribir tres líneas de
+información. La primera línea debe contener un nombre, la segunda línea un
+apellido y finalmente, la edad. A continuación se debe leer el archivo e imprimir
+por consola todas las líneas del mismo.
+
+Parámetro:
+- No recibe ningún parámetro debido a que dentro de esta función se crea un
+archivo de texto.
+
+Ejemplo:
+    Salida:
+        Juan
+        Perez
+        30
+
+Enunciat:
+Implementa la funció 'create_read_file()', que no rep cap
+paràmetre pel fet que aquesta funció ha de crear un fitxer de text
+'text_file.txt', dins d'aquest fitxer cal escriure tres línies d'
+informació. La primera línia ha de contenir un nom, la segona línia un
+cognom i, finalment, l'edat. A continuació cal llegir el fitxer i imprimir
+per consola totes les línies del mateix.
+
+Paràmetre:
+- No rep cap paràmetre pel fet que dins d'aquesta funció es crea un
+fitxer de text.
+
+Exemple:
+     Sortida:
+         Joan
+         Perez
+         30
+
+"""
+def create_read_file():
+    # Create and write to the file
+    with open("text_file.txt", "w") as f:
+        f.write("Juan\n")
+        f.write("Perez\n")
+        f.write("30\n")
+
+    # Read the file and print its contents
+    with open("text_file.txt", "r") as f:
+        for line in f:
+            print(line.strip())
+
+
+# Si quieres probar tu código, descomenta las siguientes líneas y ejecuta el script
+# Si vols provar el teu codi, descomenta les línies següents i executa l'script
+# create_read_file()
